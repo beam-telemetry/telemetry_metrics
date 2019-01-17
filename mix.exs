54184c157ed62bcdac3c8c09bcd286c4da12a591
Arkadiusz Gil <arkgil@users.noreply.github.com>
2019-01-17 15:38:43 +0100

    Release v0.1.0 (#20)

--- mix.exs
+++ mix.exs
@@ -1,14 +1,19 @@
 defmodule Telemetry.Metrics.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :telemetry_metrics,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       preferred_cli_env: preferred_cli_env(),
       deps: deps(),
+      docs: docs(),
+      description: description(),
+      package: package(),
       test_coverage: [tool: ExCoveralls]
     ]
   end
@@ -33,6 +38,30 @@ defmodule Telemetry.Metrics.MixProject do
       {:ex_doc, "~> 0.19.0", only: :docs},
       {:dialyxir, "~> 1.0.0-rc.3", only: :test, runtime: false},
       {:excoveralls, "~> 0.10.0", only: :test, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "README",
+      canonical: "http://hexdocs.pm/telemetry_metrics",
+      source_url: "https://github.com/beam-telemetry/telemetry_metrics",
+      source_ref: "v#{@version}",
+      extras: ["README.md"]
+    ]
+  end
+
+  def description do
+    """
+    Defines data model and specifications for aggregating Telemetry events.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Arkadiusz Gil", "José Valim"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/beam-telemetry/telemetry_metrics"}
     ]
   end
 end
