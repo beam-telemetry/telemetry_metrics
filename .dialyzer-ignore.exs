7aa7f0f75f0f0737ef86b18da454e04a3e2486a6
Arek Gil <arkadiusz@arkgil.net>
2021-05-19 22:47:43 +0200

    Add dialyzer ignore

---
+++ .dialyzer-ignore.exs
@@ -0,0 +1,3 @@
+[
+  {"lib/telemetry_metrics/console_reporter.ex:92:improper_list_constr List construction (cons) will produce an improper list, because its second argument is <<_::8, _::size(1)>>."}
+]
