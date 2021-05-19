c52c581537ff9b75e99496610865a94ba8e103f3
Arek Gil <arkadiusz@arkgil.net>
2021-05-19 23:07:23 +0200

    Fix ignore file name

--- .dialyzer-ignore.exs
+++
@@ -1,3 +0,0 @@
-[
-  {"lib/telemetry_metrics/console_reporter.ex:92:improper_list_constr List construction (cons) will produce an improper list, because its second argument is <<_::8, _::size(1)>>."}
-]
